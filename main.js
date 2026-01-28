e1ea69d57ed48ef7bd91e23f01a2d87156302bcc
K025C0040 <K025C0040@g.neec.ac.jp>
2026-01-28 14:23:34 +0900

    clean gabages i cannot saved

--- main.js
+++ main.js
@@ -2,66 +2,59 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// 1. シーン（舞台）を作る
+//シーン
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xafeeef); // 背景を薄いグレーに
+scene.background = new THREE.Color(0xafeeef);//背景　CSSにて
 scene.background = null;
 
-// 2. カメラを作る（画角, アスペクト比, 描画開始距離, 描画終了距離）
+//カメラ
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5; // カメラを少し後ろに引く
+camera.position.z = 5;
 
-// 3. レンダラー（現像機）を作る
+//レンダラー
 const renderer = new THREE.WebGLRenderer({ 
     antialias: true, 
-    alpha: true  // ★ 背景を透明にする設定
+    alpha: true
 });
 
-// size
+// サイズ
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement); // HTMLにキャンバスを追加
+document.body.appendChild(renderer.domElement);
 
-// orbit
+//オービット設定
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // 慣性を利かせて滑らかにする
+controls.enableDamping = true;
 
 
-// 4. ライト（照明）を置く（これがないとモデルが真っ黒！）
-// 正面からの光
+//ライト
+// key
 const topLight = new THREE.DirectionalLight(0xffffff, 3);
 topLight.position.set(5, 5, 5);
 scene.add(topLight);
 
-// 横からの青っぽい補助光（エッジが立ってカッコよくなる）
+// fill
 const sideLight = new THREE.PointLight(0xFB377E, 3);
 sideLight.position.set(5, 2, 0);
 scene.add(sideLight);
 
-// 下からの反射光
-const backLight = new THREE.PointLight(0xffffff, 0.5);
-backLight.position.set(0, -5, -2);
-scene.add(backLight);
 
-// 5. STLモデルを読み込む
+//モデル読み込み
 let myModel;
-const loader = new GLTFLoader(); // GLTFLoaderに変更
+const loader = new GLTFLoader();
 
-// 'models/your-model.stl' の部分は、自分のファイル名に書き換えてね！
 loader.load('./models/guitarforschool.glb', function (gltf) {
     
-    // GLBの場合は、gltf.scene の中にモデル（メッシュ）がすでに入っています
     const model = gltf.scene;
 
-    // サイズや向きの調整（GLBはBlenderの見た目通りに出やすいです）
+    // サイズや向きの調整
     model.scale.set(1, 1, 1); 
     model.position.set(0, -1, 0);
     
     scene.add(model);
 
-    // 全体の変数に代入して、下の animate() で回せるようにする
     myModel = model;
 
-    // Anime.js の演出もそのまま使えます！
+    //アニメーション
     anime({
         targets: model.scale,
         x: 0.3,
@@ -75,15 +68,14 @@ loader.load('./models/guitarforschool.glb', function (gltf) {
     console.error(error);
 });
 
-// パラパラ漫画のループ
+//ループ
 function animate() {
     requestAnimationFrame(animate);
     
-    // ★読み込みが終わっていたら、少しずつ回し続ける
     if (myModel) {
         myModel.rotation.y += 0.01; 
     }
-    controls.update(); // これが必要！
+    controls.update();
     
     renderer.render(scene, camera);
 }
