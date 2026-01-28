be8b02b679caf41c5c51d86e28fc1a13f266f302
K025C0040 <K025C0040@g.neec.ac.jp>
2026-01-28 14:17:31 +0900

    add background and light

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // 1. シーン（舞台）を作る
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xeeeeee); // 背景を薄いグレーに
+scene.background = new THREE.Color(0xafeeef); // 背景を薄いグレーに
+scene.background = null;
 
 // 2. カメラを作る（画角, アスペクト比, 描画開始距離, 描画終了距離）
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,14 +13,19 @@ camera.position.z = 5; // カメラを少し後ろに引く
 
 // 3. レンダラー（現像機）を作る
 const renderer = new THREE.WebGLRenderer({ 
-    antialias: false, // ギザギザ防止をオフにすると少し軽くなる
-    powerPreference: "high-performance" // GPUを本気で使わせる
+    antialias: true, 
+    alpha: true  // ★ 背景を透明にする設定
 });
 
+// size
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); // HTMLにキャンバスを追加
+
+// orbit
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // 慣性を利かせて滑らかにする
+
+
 // 4. ライト（照明）を置く（これがないとモデルが真っ黒！）
 // 正面からの光
 const topLight = new THREE.DirectionalLight(0xffffff, 4);
